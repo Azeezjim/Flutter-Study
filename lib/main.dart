f2578406b03a434f735e9816116c8059df497dee
Azeezjim <azeexjim@gmail.com>
2022-10-11 03:56:29 -1100

    flutter commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(MaterialApp(home: NinjaCard()));
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Azeezjim's App",
-          style: TextStyle(
-            fontSize: 22.0,
+      backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Ninja ID Card'),
+          backgroundColor: Colors.grey[850],
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+          child: Column(
+            children: [
+              Text(
+                "NAME",
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2, 
+                ),
+              ),
+            ],
           ),
         ),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(96, 39, 38, 38),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              child: Text('Garuba'),
-              // padding: EdgeInsets.all(20),
-              margin: EdgeInsets.all(20),
-              color:  Color.fromARGB(255, 23, 40, 56),
-            ),
-            Container(
-              // padding: EdgeInsets.all(30),
-              margin: EdgeInsets.all(30),
-              child: Text('AbdulAzeez'),
-              color:  Color.fromARGB(255, 75, 105, 136),
-            ),
-            Container(
-              child: Text('Ebalume'),
-              // padding: EdgeInsets.all(40),
-              margin: EdgeInsets.all(40),
-              color:  Color.fromARGB(255, 153, 159, 164),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text(
-          'Click',
-          style: TextStyle(
-            fontSize: 10.0,
-            fontFamily: 'Alkalami',
-          ),
-        ),
-        backgroundColor: Color.fromARGB(95, 82, 15, 15),
-      ),
     );
   }
 }
